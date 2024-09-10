173c909b5c89c1b847caa4cacacf72a1e8cb837a
Felix Nilles <felix@dienilles.de>
2024-09-10 11:11:03 +0200

    fix4?

--- src/registration/migrations/0064_alter_helper_id.py
+++ src/registration/migrations/0064_alter_helper_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registration', '0063_helper_user_id'),
+        ('registration', '0063_alter_event_url_name')
     ]
 
     operations = [
